82d2c38766e912ddb89a8313141d51b3a85d190c
fjzzhongyi <fjzzhongyi@163.com>
2017-03-11 16:08:39 +0800

    send bitrate can reach 50Mbps, client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,9 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -8,6 +11,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Date;
 
 /*
  * 客户端
@@ -20,20 +24,33 @@ public class Client {
             //2.获取输出流，向服务器端发送信息
             OutputStream os=socket.getOutputStream();//字节输出流
         	PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
-        	byte[] bt = new byte[100000];
+        	File fw= new File("/home/rqm/Documents/clientsend.output");
+        	if (!fw.exists()){
+        		fw.createNewFile();
+        	}
+        	BufferedWriter bw= new BufferedWriter(new FileWriter(fw));
+        	
+        	int btlength=10000;
+        	byte[] bt = new byte[btlength];
             int a=1;
             while (a!=0){
+            	long t1=new Date().getTime();
             	pw.write(new String(bt));
+            	pw.write('\n');
+            	long t2= new Date().getTime();
+            	bw.write(String.valueOf(t1)+' '+String.valueOf(t2)+' '+String.valueOf(btlength)+'\n');
+            	bw.flush();
             	pw.flush();
-            	a++;
+            	/*a++;
             	if (a%1000==0)
             		System.out.print(a);
             	if (a%1==0){
             		Thread.sleep(0);
-            	}
+            	}*/
             }
             socket.shutdownOutput();//关闭输出流
-        	pw.close();
+        	bw.close();
+            pw.close();
             os.close();
             //3.获取输入流，并读取服务器端的响应信息
             InputStream is=socket.getInputStream();
@@ -51,9 +68,6 @@ public class Client {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        } 
     }
 }
